580f8e529b68c1bcc2874a1a6c00cf904e9debdb
JasonPorras <porrasartaviajason@gmail.com>
2023-06-06 17:05:02 -0600

    updated

--- shopDocuments/mainShop.js
+++ shopDocuments/mainShop.js
@@ -1,9 +1,9 @@
-import { jokeRandom  } from "./printRandomJokeProduct.js";
+import { printRandomJokeById  } from "./printRandomJokeProduct.js";
 import { createdElementsRandomJoke } from "/js/creatorElementsJokes/createdRandomJoke.js";
 
-jokeRandom();
+
+printRandomJokeById();
 createdElementsRandomJoke();
 
-//we select the IDs of the DOM
-const randomButton = document.getElementById("randomButton");
-randomButton.addEventListener("click", jokeRandom);
+
+
--- shopDocuments/printRandomJokeProduct.js
+++ shopDocuments/printRandomJokeProduct.js
@@ -1,20 +1,10 @@
 import { fetchRandomJoke, getJokeById } from "/js/apiConection/apiConection.js";
 
-const id = new URLSearchParams(window.location.search).get("id");
 
-
-async function hola(){
-  const a = id;
-  const b = await getJokeById(a)
-  console.log(b,"prueba");
-}
-
-export function jokeRandom() {
-  hola()
-
-  fetchRandomJoke()
-    .then((b) => {
-      const jokes = document.getElementById("jokes");
-      jokes.innerHTML = `<p>${b.joke}</p>`;
-    });
+// Función para obtener un chiste aleatorio y mostrarlo
+export async function printRandomJokeById() {
+  const id = new URLSearchParams(window.location.search).get("id");
+  const joke = await getJokeById(id);
+  const jokes = document.getElementById("jokes");
+  jokes.innerHTML = `<p>${joke.joke}</p>`;
 }
